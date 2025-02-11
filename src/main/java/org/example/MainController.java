bbb683fc25d5764f50a8afa3a4f5d26284cac32a
Thomas Micheler <tmicheler@tgm.ac.at>
2025-02-11 06:07:02 +0100

    Update main class.

--- src/main/java/org/example/MainController.java
+++ src/main/java/org/example/MainController.java
@@ -14,6 +14,7 @@ public class MainController {
 	@PostMapping(path="/add") // Map ONLY POST Requests
 	public @ResponseBody String addNewUser (@RequestParam String name
 			, @RequestParam String email) {
+
 		// @ResponseBody means the returned String is the response, not a view name
 		// @RequestParam means it is a parameter from the GET or POST request
 
